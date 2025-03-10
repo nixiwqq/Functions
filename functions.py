ac63c528a7aab629b02c921939f70019b14d1ce2
Nika <129578493+nixiwqq@users.noreply.github.com>
2025-03-10 19:20:35 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,18 @@
+
+# task 1
+def sum(*args):
+   total_sum = 0
+   for i in args:
+       total_sum += i
+   return total_sum
+print(sum(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+
+
+#task 2
+def maxx(*args):
+   total_max = findmaxx()
+   return total_max
+print(max(1, 2, 3, 9, 5, 6, 7, 8, 4))
+
+
